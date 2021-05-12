afbc958d416ac5b8ad841ca200e78f1ce60e0f74
TC <tomas.ciernik@and.digital>
2021-05-12 09:59:35 +0100

    first commit

---
+++ C.NET samples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C.NET_samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
